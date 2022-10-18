7975e32899fc46c47533a512e479a8d200e73b37
ToluwaDavid <mailtolulopedavid@gmail.com>
2022-10-18 10:38:20 +0100

    Adding the Home, About and Store page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Store from "./pages/Store"
 
 
 function App() {
-
-
   return (
-    <h1>Hi</h1>
+    <>
+      <Container>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/store' element={<Store />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </Container>
+    </>
   )
 }
 
