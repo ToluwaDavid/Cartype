2067784bfffaaa60c192adcd3712664b3b909305
ToluwaDavid <mailtolulopedavid@gmail.com>
2022-10-18 16:42:33 +0100

    Card components of the store page

--- src/components/StoreItems.tsx
+++ src/components/StoreItems.tsx
@@ -3,7 +3,7 @@ import { Card } from 'react-bootstrap'
 type StoreItemProps = {
     id: number
     name: string
-    price: Number
+    price: number
     imgUrl: string
 }
 
@@ -13,8 +13,9 @@ const StoreItems = ({ id, name, price, imgUrl }: StoreItemProps) => {
             <Card>
                 <Card.Img variant="top" src={imgUrl} height="200px" style={{ objectFit: "cover" }} />
                 <Card.Body className='d-flex flex-column'>
-                    <Card.Title>
-
+                    <Card.Title className='d-flex justify-content-between align-items-baseline mb-4'>
+                        <span className='fs-2'>{name}</span>
+                        <span className='ms-2 text-muted'>{price}</span>
                     </Card.Title>
                 </Card.Body>
             </Card>
